6b8bfb39d03f987de7b110ab5b6647cb7eb110c1
Jose Juan <thronks@gmail.com>
2025-07-11 08:01:15 -0600

    cabmio 7

--- creararchivo.py
+++ creararchivo.py
@@ -1,11 +1,11 @@
 # Nombre del archivo
-nombre_archivo = "ejemplo5.txt"
+nombre_archivo = "ejemplo6.txt"
 
 # Contenido que quieres escribir
-contenido = "Hola, este es un archivo de texto creado con Python."
+contenido = "Hola, este es un archivo de texto creado con Python version 6."
 
 # Crear y escribir en el archivo
 with open(nombre_archivo, "w", encoding="utf-8") as archivo:
     archivo.write(contenido)
 
-print(f"Archivo '{nombre_archivo}' creado con éxito.")
+print(f"Archivo '{nombre_archivo}' creado con éxito cambio 6.")
